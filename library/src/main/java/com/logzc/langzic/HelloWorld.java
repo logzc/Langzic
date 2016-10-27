8acaf76365075f734db698e399fd55e9fa399990
Zic <logzcx@126.com>
2016-10-27 14:39:03 +0800

    Finish initializing the project.

---
+++ library/src/main/java/com/logzc/langzic/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.logzc.langzic;
+
+/**
+ * Created by lishuang on 2016/10/27.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
